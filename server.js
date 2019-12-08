78fd45130c2d2a263f9835589b6b5f96393523da
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2019-12-08 11:02:06 -0800

    Basic server files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+var _ = require('lodash');
+
+
+server.get("/json", (req, res) => {
+   res.json({ message: "Hello world" });
+});
+
+server.get("/", (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+});
+
+
+const port = 4000;
+
+server.listen(port, () => {
+
+    var naughty_or_nice = ['Naughty','Nice'];
+    console.log(`Ho ho ho! Father Christmas has you on his list of ${_.sample(naughty_or_nice)} Kids, for using ${port}`);
+});
