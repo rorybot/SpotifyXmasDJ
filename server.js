23908f7c2499d9c998321b81aa1a694627412476
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2019-12-10 16:55:20 -0800

    Barebones React app achieved

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+const port = process.env.PORT || 4000;
 var _ = require('lodash');
 
 
@@ -7,19 +8,20 @@ server.get("/json", (req, res) => {
    res.json({ message: "Hello world" });
 });
 
-server.get("/", (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+// server.get("/", (req, res) => {
+//    res.sendFile(__dirname + '/client/public/index.html');
+// });
+
+
+//
+// server.get('/spotify_auth', (req, res) => res.send({ username: process.env.XMAS_SPOTIFY_ID }));
+
+server.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT!!' });
 });
 
-server.get("/app", (req, res) => {
-   res.sendFile(__dirname + '/app/app.js');
-});
-
-
-const port = 4000;
 
 server.listen(port, () => {
-
     var naughty_or_nice = ['Naughty','Nice'];
     console.log(`Ho ho ho! Father Christmas has you on his list of ${_.sample(naughty_or_nice)} Kids, for using ${port}`);
 });
