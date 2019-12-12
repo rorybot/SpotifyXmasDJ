191cd210e6d4fc3caa1b9e13cd4a5d6089206bf4
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2019-12-12 15:09:44 -0800

    prettified

--- server.js
+++ server.js
@@ -1,107 +1,121 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const mysql = require('mysql');
+const mysql = require("mysql");
 const options = {
-    user: 'root',
-    password:'',
-    database:'spotify_xmas_dj'
-}
+  user: "root",
+  password: "",
+  database: "spotify_xmas_dj"
+};
 const connection = mysql.createConnection(options);
 const port = process.env.PORT || 4000;
-var _ = require('lodash');
-const client_id =  process.env.SPOTIFY_XMAS_ID; // Your client id
+var _ = require("lodash");
+const client_id = process.env.SPOTIFY_XMAS_ID; // Your client id
 const client_secret = process.env.SPOTIFY_XMAS_SECRET; // Your secret
 // var stateKey = 'spotify_auth_state';
-var mustache = require('mustache');
-var fs = require('fs');
-var querystring = require('querystring');
-var request = require('request');
-var redirect = 'https://cd524f14.ngrok.io/callback';var view = {
-  auth_url: 'https://accounts.spotify.com/authorize?client_id='+client_id+'&response_type=code&redirect_uri='+redirect+'&scope=user-read-private%20user-read-email'
+var mustache = require("mustache");
+var fs = require("fs");
+var querystring = require("querystring");
+var request = require("request");
+var redirect = "https://cd524f14.ngrok.io/callback";
+var view = {
+  auth_url:
+    "https://accounts.spotify.com/authorize?client_id=" +
+    client_id +
+    "&response_type=code&redirect_uri=" +
+    redirect +
+    "&scope=user-read-private%20user-read-email"
 };
-var template = fs.readFileSync('./public/index.html', "utf8");
+var template = fs.readFileSync("./public/index.html", "utf8");
 
-
-connection.connect(err=>{
-  if(err){
-    console.error('Error connecting to db');
+connection.connect(err => {
+  if (err) {
+    console.error("Error connecting to db");
     throw err;
   }
-})
-
-
-server.get("/", (req, res) => {
-      res.send(mustache.to_html(template, view));
 });
 
-server.get('/spotify_backend', (req, res) => {
+server.get("/", (req, res) => {
+  res.send(mustache.to_html(template, view));
+});
+
+server.get("/spotify_backend", (req, res) => {
   res.send({
     client_id: client_id,
     client_secret: client_secret
   });
 });
 
-server.get('/callback', function(req, res) {
+server.get("/callback", function(req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
   // var storedState = req.cookies ? req.cookies[stateKey] : null;
-    // res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+  // res.clearCookie(stateKey);
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: redirect,
+      grant_type: "authorization_code"
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64")
+    },
+    json: true
+  };
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var access_token = body.access_token;
-        var refresh_token = body.refresh_token;
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-        function userQuery(callback){
-          request.get(options, function(error, response, body) {
-            callback(body.id,body.email);
-          });
-        };
-        function storeUserData(user_id, user_email){
-          connection.query('INSERT INTO users (id,email,auth_token,refresh_token) VALUES (?,?,?,?)',[user_id,user_email,access_token,refresh_token], (error,todos,fields)=>{
-            if(error){
-              console.error('An error in query');
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      var refresh_token = body.refresh_token;
+      var options = {
+        url: "https://api.spotify.com/v1/me",
+        headers: { Authorization: "Bearer " + access_token },
+        json: true
+      };
+      function userQuery(callback) {
+        request.get(options, function(error, response, body) {
+          callback(body.id, body.email);
+        });
+      }
+      function storeUserData(user_id, user_email) {
+        connection.query(
+          "INSERT INTO users (id,email,auth_token,refresh_token) VALUES (?,?,?,?)",
+          [user_id, user_email, access_token, refresh_token],
+          (error, todos, fields) => {
+            if (error) {
+              console.error("An error in query");
               throw error;
             }
             console.log("Successful entry");
-          } )
-        }
-        userQuery(storeUserData);
-        res.redirect('/#');
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
+          }
+        );
       }
-    });
+      userQuery(storeUserData);
+      res.redirect("/#");
+    } else {
+      res.redirect(
+        "/#" +
+          querystring.stringify({
+            error: "invalid_token"
+          })
+      );
+    }
+  });
 });
 
-
-server.get('/refresh_token', function(req, res) {
-  // requesting access token from refresh token
+server.get("/refresh_token", function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64")
+    },
     form: {
-      grant_type: 'refresh_token',
+      grant_type: "refresh_token",
       refresh_token: refresh_token
     },
     json: true
@@ -111,13 +125,17 @@ server.get('/refresh_token', function(req, res) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
-        'access_token': access_token
+        access_token: access_token
       });
     }
   });
 });
 
 server.listen(port, () => {
-    var naughty_or_nice = ['Naughty','Nice'];
-    console.log(`Ho ho ho! Father Christmas has you on his list of ${_.sample(naughty_or_nice)} Kids, for using ${port}`);
+  var naughty_or_nice = ["Naughty", "Nice"];
+  console.log(
+    `Ho ho ho! Father Christmas has you on his list of ${_.sample(
+      naughty_or_nice
+    )} Kids, for using ${port}`
+  );
 });
