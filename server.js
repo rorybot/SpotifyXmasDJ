a1645b3ef094b0b90b7a4b2887b7cc4549fd2f85
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2019-12-12 15:07:06 -0800

    cleaned up code

--- server.js
+++ server.js
@@ -16,26 +16,11 @@ var mustache = require('mustache');
 var fs = require('fs');
 var querystring = require('querystring');
 var request = require('request');
-var redirect = 'https://cd524f14.ngrok.io/callback';
-
-// var $ = function(){};
-
-server.get('/spotify_backend', (req, res) => {
-  res.send({
-    client_id: client_id,
-    client_secret: client_secret
-  });
-});
-
-var view = {
+var redirect = 'https://cd524f14.ngrok.io/callback';var view = {
   auth_url: 'https://accounts.spotify.com/authorize?client_id='+client_id+'&response_type=code&redirect_uri='+redirect+'&scope=user-read-private%20user-read-email'
 };
-
 var template = fs.readFileSync('./public/index.html', "utf8");
 
-server.get("/", (req, res) => {
-      res.send(mustache.to_html(template, view));
-});
 
 connection.connect(err=>{
   if(err){
@@ -44,12 +29,22 @@ connection.connect(err=>{
   }
 })
 
-server.get('/callback', function(req, res) {
 
+server.get("/", (req, res) => {
+      res.send(mustache.to_html(template, view));
+});
+
+server.get('/spotify_backend', (req, res) => {
+  res.send({
+    client_id: client_id,
+    client_secret: client_secret
+  });
+});
+
+server.get('/callback', function(req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
   // var storedState = req.cookies ? req.cookies[stateKey] : null;
-
     // res.clearCookie(stateKey);
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
@@ -66,49 +61,29 @@ server.get('/callback', function(req, res) {
 
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-
         var access_token = body.access_token;
         var refresh_token = body.refresh_token;
-
-
         var options = {
           url: 'https://api.spotify.com/v1/me',
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         };
-
-        // use the access token to access the Spotify Web API
-
         function userQuery(callback){
           request.get(options, function(error, response, body) {
             callback(body.id,body.email);
           });
         };
-
-
-
         function storeUserData(user_id, user_email){
           connection.query('INSERT INTO users (id,email,auth_token,refresh_token) VALUES (?,?,?,?)',[user_id,user_email,access_token,refresh_token], (error,todos,fields)=>{
             if(error){
               console.error('An error in query');
               throw error;
             }
-            // console.log(users);
+            console.log("Successful entry");
           } )
         }
-
         userQuery(storeUserData);
-
-
-
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-
+        res.redirect('/#');
       } else {
         res.redirect('/#' +
           querystring.stringify({
@@ -116,7 +91,6 @@ server.get('/callback', function(req, res) {
           }));
       }
     });
-  // }
 });
 
 
