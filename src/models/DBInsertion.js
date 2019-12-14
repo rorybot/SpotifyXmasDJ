0d415d7dd122ef934cdc598be4045c6c5b894aba
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2019-12-14 13:47:38 -0800

    Model for handling database work; handles insertion of mixed playlist

--- src/models/DBInsertion.js
+++ src/models/DBInsertion.js
@@ -1,9 +1,47 @@
-(function(exports) {
-  function DBInsertion() {}
+const mysql = require("mysql");
+const options = {
+  user: "root",
+  password: "",
+  database: "spotify_xmas_dj"
+};
+const connection = mysql.createConnection(options);
 
-  DBInsertion.prototype.insertSongs = function() {
+module.exports = class DBInsertion {
+  constructor() {
+  }
 
-  };
+  createPlaylistMeta(user){
+    return new Promise(function(resolve,reject){
+      connection.query(
+        "INSERT INTO mixed_playlist_meta (name,user) VALUES (?,?)",
+        ['Test',user],
+        (error, playlists, fields) => {
+          if (error) {
+            reject(error);
+          }
+          resolve(playlists)
+        }
+      );
 
-  exports.DBInsertion = DBInsertion;
-})(this);
+    })
+
+  }
+
+  insertMixedPlaylist(playlistID,allMusicArray){
+    console.log(allMusicArray)
+    allMusicArray.forEach(function(song){
+
+      connection.query(
+        "INSERT INTO mixed_playlist (track,popularity,playlist_id) VALUES (?,?,?)",
+        [song.track_id,song.popularity,playlistID],
+        (error, new_playlist, fields) => {
+          if (error) {
+            throw error;
+          }
+          return JSON.stringify(new_playlist)
+        }
+      );
+    })
+  }
+
+}
