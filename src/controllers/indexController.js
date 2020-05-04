25da84e7a27435882638af64642bc080a143d1f0
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-05-04 13:52:14 -0700

    Fixed recursive call to generate playlist list

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,78 +1,68 @@
 const request = require("request");
 const mustache = require("mustache");
 const fs = require("fs");
-const template = fs.readFileSync(__dirname + "/../../public/template/index.html", "utf8");
+const template = fs.readFileSync(
+  __dirname + "/../../public/template/index.html",
+  "utf8"
+);
 
 let redirect = "http://localhost:4000/callback";
 let view = {};
 
-
-exports.getIndexFile = (req,res) => {
-  console.log("LOL")
+exports.getIndexFile = async (req, res) => {
+  console.log("LOL");
   if (!req.cookies.authenticated) {
-    view.auth_url = '/spotifyAuth';
+    view.auth_url = "/spotifyAuth";
     res.send(mustache.to_html(template, view));
   } else {
     console.log("Authenticated");
-    //Move this responsibiltiy to a view-adjacent js file
-    //Need to send a reauthentication link if missing
+
     var playlistArray = [];
-    //check if token needs refreshing
 
+    view.auth_url = "#choosePlaylists";
 
-    // testTokenForRefresh(req.cookies.authenticated).then(function(user) {
-      // console.log(user)
-        //returns a user item if valid and refreshed; pass to spotify, get playlists, add to rendering template
-        // view.display_on_auth = "display:none";
-        view.auth_url = "#choosePlaylists";
-        getPlaylistsFromURL(req.user, playlistArray, function() {
-          console.log(req)
-          res.send(mustache.to_html(template, view));
-        });
-    // }).catch(function(){
-        //renders page without filtering any user details through// cookie doesn't match existing DB user
-        // res.send(mustache.to_html(template, view));
-    // });
-
-
-  }
-}
-
-function getPlaylistsFromURL(user,playlistArray,callback = false,newURL=false) {
-  console.log(2)
-  var options = {
-    url: "https://api.spotify.com/v1/users/" + user.id + "/playlists",
-    headers: { Authorization: "Bearer " + user.auth_token },
-    json: true
-  };
-  if(newURL){
-    options.url = newURL;
-  }
-  request.get(options, function(error, response, body) {
-    if(body.error){ console.log(body.error); return calback()}
-      console.log('called')
-    body.items.forEach(function(playlist) {
-      playlistArray.push({
-        url: `${playlist.external_urls.spotify} `,
-        name: `${playlist.name} `,
-        id: `${playlist.id}`
+    await getPlaylistsFromURL(req.user).then(finalResult => {
+      let final = finalResult.map(function(playlist) {
+        return `<option value='${playlist.id}' >${playlist.name}</option>`;
       });
+      view.playlistList = final.join("");
+      res.send(mustache.to_html(template, view));
     });
-    view.playlistList = "";
-    if (body.next) {
-      getPlaylistsFromURL(user, playlistArray, callback,body.next);
-    } else {
-      //
-      playlistArray.forEach(function(playlist) {
-        view.playlistList += `<option value='${playlist.id}' >${
-          playlist.name
-        }</option>`;
-      });
-      callback();
+  }
+};
+
+function getPlaylistsFromURL(user, newURL = false, playlistArray = []) {
+  return new Promise(function(resolve, reject) {
+    console.log("Go");
+
+    var options = {
+      url: "https://api.spotify.com/v1/users/" + user.id + "/playlists",
+      headers: { Authorization: "Bearer " + user.auth_token },
+      json: true
+    };
+    if (newURL) {
+      options.url = newURL;
     }
+
+    request.get(options, function(error, response, body) {
+      if (body.error) {
+        console.log(body.error);
+        return "Error";
+      }
+      let listChunk = body.items.map(function(playlist) {
+        return {
+          url: `${playlist.external_urls.spotify} `,
+          name: `${playlist.name} `,
+          id: `${playlist.id}`
+        };
+      });
+      playlistArray.push(listChunk);
+
+      if (body.next) {
+        resolve(getPlaylistsFromURL(user, body.next, playlistArray));
+      } else {
+        resolve(playlistArray.flat());
+      }
+    });
   });
 }
-
-// function testTokenForRefresh(userID) {
-//
-// }
